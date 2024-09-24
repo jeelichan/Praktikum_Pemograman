62e481ed944d5a53c13f4b193ad70e3a58dcbbb5
jeelichan <114078447+jeelichan@users.noreply.github.com>
2024-09-24 11:58:05 +0000

    rename variables

--- Pertemuan 5/Soal 1/fizzBuzz.cpp
+++ Pertemuan 5/Soal 1/fizzBuzz.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 
 int main(){
-    vector<string> arr{101};
+    vector<string> vec{101};
     for(int i=1;i<=100;i++){
-        if( i%3==0 ) arr[i]+="Fizz";
-        if( i%5==0 ) arr[i]+="Buzz";
-        if( i%3!=0 && i%5!=0 ) arr[i]=to_string(i);
+        if( i%3==0 ) vec[i]+="Fizz";
+        if( i%5==0 ) vec[i]+="Buzz";
+        if( i%3!=0 && i%5!=0 ) vec[i]=to_string(i);
     }
-    for(int i=1;i<=100;i++) cout<<arr[i]<<endl;
+    for(int i=1;i<=100;i++) cout<<vec[i]<<endl;
 }
 
