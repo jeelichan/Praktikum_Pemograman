dbf21edae3d282c9548c0e158cff355668f57053
jeelichan <114078447+jeelichan@users.noreply.github.com>
2024-11-05 07:10:05 +0000

    fibo

--- Pertemuan 8/fibo.cpp
+++ Pertemuan 8/fibo.cpp
@@ -1,1 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int fibo(int n){
+    if(n==1) return 0;
+    if(n==2) return 1;
+    else{
+        return fibo(n-1) + fibo(n-2);
+    }
+}
+ 
+int main(){
+    int n
+}
