40ccf8f11411f677a79c25182427202f999fcb1c
jeelichan <114078447+jeelichan@users.noreply.github.com>
2024-11-26 08:11:40 +0000

    adding comments, rename folder

--- Responsi/Soal 1/luasLingkaran.cpp
+++ Responsi/Soal 1/luasLingkaran.cpp
@@ -8,11 +8,11 @@ int main(){
     cout<<"Masukkan radius lingkaran:";
     cin>>radius;
 
+    //Cek apakah input valid atau tidak
     if(radius<0){
         cout<<"Input tidak valid"<<endl;
         return 0;
     }
-
 
     //Luas = pi * radius * radius
     float luas = pi*radius*radius;
--- Responsi/Soal 4/maxNumber.cpp
+++ Responsi/Soal 4/maxNumber.cpp
@@ -4,29 +4,35 @@ using namespace std;
 int main(){
     int banyakData;
 
+    //Masukkan jumlah data  
     cout<<"Masukkan banyak data: "; 
     cin>>banyakData;
     cout<<endl;
 
+    //Cek apakah input valid
     if(banyakData<=0){
         cout<<"Input tidak valid"<<endl;
         return 0;
     }
 
+    //Inisialisasi array integer dengan jumlah data sebanyak banyakData
     int bilangan[banyakData];
 
+    //Masukkan data
     for(int i=0; i<banyakData;i++){
         cout<<"Masukkan data ke-"<<i+1<<": ";
         cin>>bilangan[i];
     }
 
+    //Inisialisasikan nilaiMax dengan elemen pertama pada array bilangan
     int nilaiMax=bilangan[0];
 
+    //Perbarui nilaiMax jika ditemukan nilai yang lebih besar pada daftar 
     for(int i = 1; i < banyakData; i++){
         if(bilangan[i]>nilaiMax) nilaiMax = bilangan[i];
     }
 
-
+    //Cetak nilai maks
     cout<<"\nNilai maksimum dalam daftar adalah "<<nilaiMax<<endl;
 
     return 0;
